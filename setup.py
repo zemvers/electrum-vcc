827a38292fa95759a02779a076a7106c629dbfed
James Lovejoy <jameslovejoy1@gmail.com>
2017-07-12 15:15:02 +0100

    Fix bug where the vtc module was missing after install

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'electrum_ltc',
         'electrum_ltc_gui',
         'electrum_ltc_gui.qt',
+        'electrum_ltc_gui.vtc',
         'electrum_ltc_plugins',
         'electrum_ltc_plugins.audio_modem',
         'electrum_ltc_plugins.cosigner_pool',
