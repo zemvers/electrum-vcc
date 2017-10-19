408b454edc594f71bc6bd2a2f4bd55b329acb940
Luke Childs <lukechilds123@gmail.com>
2017-10-19 11:19:48 +0700

    Add vtc.lukechilds.co node
    Via https://github.com/lukechilds/docker-electrum-vertcoin

--- lib/network.py
+++ lib/network.py
@@ -54,6 +54,7 @@ DEFAULT_SERVERS = {
     'fr1.vtconline.org': DEFAULT_PORTS,
     'uk1.vtconline.org': DEFAULT_PORTS,
     'vtc.horriblecoders.com': DEFAULT_PORTS,
+    'vtc.lukechilds.co': DEFAULT_PORTS,
 }
 
 '''
